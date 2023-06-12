51c8a92dc687d5dca609043d90ed44a46c350a9c
Brenlify <robles.167967@gmail.com>
2023-06-12 19:25:42 +0800

    testing

--- reg_inserts/add-seats.php
+++ reg_inserts/add-seats.php
@@ -214,7 +214,7 @@ $id = $conn->query($sql);
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
-
+ 
 
 </body>
 
