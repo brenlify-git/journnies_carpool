aa83b3c1d30868644c6c8679bb3df06e1d879a96
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-06-12 22:53:05 +0800

    Booking

--- dashboards/dashboard-passenger.php
+++ dashboards/dashboard-passenger.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
 // session_start();
 include '../config/connection.php';
 
 ?>
 
-  
+
 <!DOCTYPE html>
 <html lang="en">
- 
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -23,9 +23,8 @@ include '../config/connection.php';
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-    rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
   <link href="../assets/css/style.css" rel="stylesheet">
 
@@ -34,7 +33,7 @@ include '../config/connection.php';
 <body>
 
   <!-- ======= Sidebar and Header ======= -->
-  <?php include '../headerbars/headerbar-passenger.php';?>
+  <?php include '../headerbars/headerbar-passenger.php'; ?>
   <?php include '../sidebars/sidebar-passenger.php';
   $idLoggedUser = $_SESSION['userID'];
 
@@ -43,143 +42,148 @@ include '../config/connection.php';
 
   ?>
 
-
-  <!-- End Sidebar and Header-->
-
-
-  <main id="main" class="main">
-
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-
-    </div><!-- End Page Title -->
-
-    <section class="section dashboard">
-      <div class="row">
-
-       <!-- Left side columns -->
-       <div class="col-lg-12 ">
-          <div class="row">
-
-         <h1 style="text-align:center">Welcome <b><?= $_SESSION['firstName'] ?></b>  to your dashboard!</h1> 
-
-         <?php
-
-        while($userGuide = mysqli_fetch_assoc($idx)): 
-          $money = $userGuide['uGems'];
-
-          $formatted_money = number_format($money, 2, '.', ',');
-          $formatted_money_with_currency = '₱' . $formatted_money;
-
   
-         ?>
-         <h3 style="text-align:center">Your total balance: <b><?= $formatted_money_with_currency ?></b> </h3>
+    <!-- End Sidebar and Header-->
 
 
-         <?php
-         
-        endwhile;
-         ?>
+    <main id="main" class="main">
 
-         <br> <br>
+      <div class="pagetitle">
+        <h1>Dashboard</h1>
+        <nav>
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </nav>
 
-         <div class="col-lg-12">
-          <div class="row mt-4">
+      </div><!-- End Page Title -->
 
-         <!-- Sales Card -->
-         <div class="col-xxl-4 col-md-12">
-              <div class="card info-card sales-card">
+      <section class="section dashboard">
+        <div class="row">
+
+          <!-- Left side columns -->
+          <div class="col-lg-12 ">
+            <div class="row">
+
+              <h1 style="text-align:center">Welcome <b><?= $_SESSION['firstName'] ?></b> to your dashboard!</h1>
+
+              <?php
+
+              while ($userGuide = mysqli_fetch_assoc($idx)) :
+                $money = $userGuide['uGems'];
+
+                $formatted_money = number_format($money, 2, '.', ',');
+                $formatted_money_with_currency = '₱' . $formatted_money;
 
 
+              ?>
+                <h3 style="text-align:center">Your total balance: <b><?= $formatted_money_with_currency ?></b> </h3>
 
-                <div class="card-body">
-                  <h5 class="card-title">Available Book <span>| As of Today</span></h5>
 
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-journal-check"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6> 245 </h6>
-                      <span class="text-success small pt-1 fw-bold">ADMIN</span> <span
-                        class="text-muted small pt-2 ps-1">VIEW</span>
+              <?php
 
-                    </div>
+              endwhile;
+              ?>
+
+              <br> <br>
+
+              <div class="col-lg-12">
+                <div class="row mt-4">
+
+                <form action="../reg_inserts/booking.php" method="POST">
+
+                  <!-- Sales Card -->
+                  <!-- Left side columns -->
+                  <div class="col-lg-12 ">
+                    <div class="row">
+                      <br> <br>
+                      <div class="col-lg-12">
+                        <div class="row mt-4">
+
+                          <?php
+
+                          $sqlCar = "SELECT * FROM route INNER JOIN car_details ON route.carID = car_details.carID INNER JOIN user ON
+                    car_details.uID = user.uID";
+                          $resultCar = $conn->query($sqlCar);
+
+                          ?>
+
+
+                          <!-- Sales Card -->
+                          <?php
+                          $x = 1;
+                          while ($row = mysqli_fetch_assoc($resultCar)) :
+
+                            $dateTime = new DateTime($row['routeDepartureTime']);
+                            $formattedDateTime = $dateTime->format("F d, Y \t g:i A");
+
+                            // Starting Point
+                            $text = $row['routeStartingPoint'];
+                            $maxCharacters = 30;
+
+                            if (strlen($text) > $maxCharacters) {
+                              $shortenedText = substr($text, 0, $maxCharacters) . "...";
+                            } else {
+                              $shortenedText = $text;
+                            }
+
+                            //End Point
+                            $text2 = $row['routeEndPoint'];
+                            $maxCharacters2 = 30;
+
+                            if (strlen($text2) > $maxCharacters2) {
+                              $shortenedText2 = substr($text2, 0, $maxCharacters2) . "...";
+                            } else {
+                              $shortenedText2 = $text2;
+                            }
+                          ?>
+                            <div class="col-xxl-4 col-md-12">
+                              <div class="card info-card sales-card">
+                                <div class="card-body">
+                                  <h5 class="card-title">Book <span>| <?= $formattedDateTime ?></span></h5>
+                                  <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                      <i class="bi bi-ev-front-fill"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                      <h6> <?= $row['carType'] ?> </h6>
+                                      <span><b>Starting Point:</b> <?= $shortenedText ?></span>
+                                      <span><b>End Point:</b> <?= $shortenedText2 ?></span><br> <br>
+                                      <input type="hidden" name="user" value="<?= $row['uID'] ?>">
+                                      <input type="text" name="router" value="<?= $row['routeID']?>">
+                                      <span class="text-success small pt-1 fw-bold"><button type="submit" class="btn btn-primary" id="accept-button" name="accept">See More</button></span>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+
+                              </div>
+                            </div><!-- End Sales Card -->
+                          <?php
+                            $x++;
+                          endwhile;
+                          ?>
+                        </div>
+                      </div>
+                    </div><!-- End Right side columns -->
                   </div>
                 </div>
-
-              </div>
-            </div><!-- End Sales Card -->
-
-            <!-- Sales Card -->
-          <div class="col-xxl-4 col-md-12">
-              <div class="card info-card sales-card">
-
-
-
-                <div class="card-body">
-                  <h5 class="card-title">Available Book <span>| As of Today</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-journal-check"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6> 245 </h6>
-                      <span class="text-success small pt-1 fw-bold">ADMIN</span> <span
-                        class="text-muted small pt-2 ps-1">VIEW</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Sales Card -->
-            <!-- Sales Card -->
-          <div class="col-xxl-4 col-md-12">
-              <div class="card info-card sales-card">
-
-
-
-                <div class="card-body">
-                  <h5 class="card-title">Available Book <span>| As of Today</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-journal-check"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6> 245 </h6>
-                      <span class="text-success small pt-1 fw-bold">ADMIN</span> <span
-                        class="text-muted small pt-2 ps-1">VIEW</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Sales Card -->
-          </div>
+                </form>
+              </div><!-- End Right side columns -->
             </div>
-        </div><!-- End Right side columns -->
-      </div>
-    </section>
+      </section>
 
-  </main><!-- End #main -->
+    </main><!-- End #main -->
+  
+
 
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
 
   </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
